5b3a74ca4bdb30ca3c2fa8d78647550a56fcf49c
dsrahul <dsrahul@yahoo.com>
2013-09-13 16:24:09 +0100

    Update support-sqls.sql
    available services

--- support-sqls.sql
+++ support-sqls.sql
@@ -15,3 +15,29 @@ WITH RPL (LEVEL, ASG_ID, PARENT_ASG_ID, NAME)
 SELECT LEVEL, 
        ASG_ID,PARENT_ASG_ID , NAME
 FROM   RPL WITH UR
+
+
+--AVAILABLE SERVICES
+SELECT A.id, 
+       A.des_serv_grp_id, 
+       A.name, 
+       A.description, 
+       A.volume, 
+       A.charge, 
+       A.product_association, 
+       A.time_to_deliver 
+FROM   tdeserv A, 
+       tdespts D, 
+       tdespar E, 
+       tdespro F, 
+       tdesass G 
+WHERE  A.id = D.del_ex_serv_id 
+       AND D.prod_type_id IN ( 'zJ5a6h+w' ) 
+       AND A.id = E.del_ex_serv_id 
+       AND E.del_ex_serv_prov_id = F.provider_id 
+       AND F.provider_type IN ( 'G' ) 
+       AND E.pair_id = G.del_ex_serv_pair_id 
+       AND G.branch_num IN ( 12 ) 
+       AND G.start_date <= CURRENT_DATE 
+       AND ( G.end_date >= CURRENT_DATE ) 
+WITH UR 
